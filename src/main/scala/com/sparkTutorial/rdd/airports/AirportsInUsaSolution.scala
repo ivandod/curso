5a3638cc7f1009691e9b43a505b324f03bdace3f
Iván Valderrey <i.valderrey@indizen.com>
2018-12-14 14:44:19 +0100

    AirPORTS  USA MEJORADO
    Se mejora el código para que sea reprocesable y se muestran sólo los ERRORES.

--- src/main/scala/com/sparkTutorial/rdd/airports/AirportsInUsaSolution.scala
+++ src/main/scala/com/sparkTutorial/rdd/airports/AirportsInUsaSolution.scala
@@ -1,14 +1,26 @@
 package com.sparkTutorial.rdd.airports
 
+import java.io
+
 import com.sparkTutorial.commons.Utils
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.reflect.io.Directory
+import scala.reflect.io.File
 
 object AirportsInUsaSolution {
 
   def main(args: Array[String]) {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    printf("Empezando JOB... \n")
 
     val conf = new SparkConf().setAppName("airports").setMaster("local[2]")
     val sc = new SparkContext(conf)
+    printf("Borramos ficheros existentes... \n")
+    val path = new Directory(new io.File("out/airports_in_usa.text"))
+    path.deleteRecursively()
 
     val airports = sc.textFile("in/airports.text")
     val airportsInUSA = airports.filter(line => line.split(Utils.COMMA_DELIMITER)(3) == "\"United States\"")
@@ -18,5 +30,6 @@ object AirportsInUsaSolution {
       splits(1) + ", " + splits(2)
     })
     airportsNameAndCityNames.saveAsTextFile("out/airports_in_usa.text")
+    printf("Fichero guardado en OUT/")
   }
 }
